fd00579e133cb1781f1a4f86627223b090af8612
iamgauravkha <gauravk@oplify.in>
2026-01-09 12:10:20 +0530

    feat: update greeting message to PWA V3 and add user name display

--- src/App.jsx
+++ src/App.jsx
@@ -12,12 +12,13 @@ function App() {
   }, []);
 
   const sayHello = () => {
-    alert("Hello from PWA V2");
+    alert("Hello from PWA V3");
   };
 
   return (
     <>
       <h1>Hello this is pwa version 1</h1>
+      <h1>Gaurav</h1>
       <button onClick={() => sayHello()}>Click me for gretting</button>
       {showUpdate && (
         <div className="fixed w-[300px] h-[100px] flex items-start flex-col justify-between bottom-4 right-4 bg-black text-white p-4 rounded">
